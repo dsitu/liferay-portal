fb807f95c2229847d10d593ed2cdcdab7dc11a36
Danny Situ <danny.situ@liferay.com>
2020-09-08 18:09:49 -0700

    COMMERCE-4741 Format log messages

--- modules/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/internal/upgrade/CommercePriceListUpgradeStepRegistrator.java
+++ modules/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/internal/upgrade/CommercePriceListUpgradeStepRegistrator.java
@@ -46,7 +46,7 @@ public class CommercePriceListUpgradeStepRegistrator
 	@Override
 	public void register(Registry registry) {
 		if (_log.isInfoEnabled()) {
-			_log.info("COMMERCE PRICE LIST UPGRADE STEP REGISTRATOR STARTED");
+			_log.info("Commerce price list upgrade step registrator started");
 		}
 
 		registry.register(
@@ -91,7 +91,7 @@ public class CommercePriceListUpgradeStepRegistrator
 		registry.register("2.1.2", "2.1.3", new DummyUpgradeProcess());
 
 		if (_log.isInfoEnabled()) {
-			_log.info("COMMERCE PRICE LIST UPGRADE STEP REGISTRATOR FINISHED");
+			_log.info("Commerce price list upgrade step registrator finished");
 		}
 	}
 
