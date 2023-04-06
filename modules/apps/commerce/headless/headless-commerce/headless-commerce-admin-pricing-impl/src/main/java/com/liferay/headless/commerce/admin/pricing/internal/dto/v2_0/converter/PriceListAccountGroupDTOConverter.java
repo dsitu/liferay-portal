38d3ca37b6aeb09ce1c7a02873128668f27664a9
Brian I. Kim <il.kim@liferay.com>
2023-04-05 19:01:34 -0700

    COMMERCE-11017 Review CommerceAccount* removal part one

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/dto/v2_0/converter/PriceListAccountGroupDTOConverter.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/dto/v2_0/converter/PriceListAccountGroupDTOConverter.java
@@ -15,6 +15,7 @@
 package com.liferay.headless.commerce.admin.pricing.internal.dto.v2_0.converter;
 
 import com.liferay.account.model.AccountGroup;
+import com.liferay.account.service.AccountGroupLocalService;
 import com.liferay.commerce.price.list.model.CommercePriceList;
 import com.liferay.commerce.price.list.model.CommercePriceListCommerceAccountGroupRel;
 import com.liferay.commerce.price.list.service.CommercePriceListCommerceAccountGroupRelService;
@@ -51,8 +52,9 @@ public class PriceListAccountGroupDTOConverter
 					getCommercePriceListCommerceAccountGroupRel(
 						(Long)dtoConverterContext.getId());
 
-		AccountGroup accountGroup =
-			commercePriceListCommerceAccountGroupRel.getAccountGroup();
+		AccountGroup accountGroup = _accountGroupLocalService.getAccountGroup(
+			commercePriceListCommerceAccountGroupRel.
+				getCommerceAccountGroupId());
 		CommercePriceList commercePriceList =
 			commercePriceListCommerceAccountGroupRel.getCommercePriceList();
 
@@ -72,6 +74,9 @@ public class PriceListAccountGroupDTOConverter
 			}
 		};
 	}
+
+	@Reference
+	private AccountGroupLocalService _accountGroupLocalService;
 
 	@Reference
 	private CommercePriceListCommerceAccountGroupRelService
