2718fc67ebd86265002cffb1a68d816efea48fa1
Danny Situ <danny.situ@liferay.com>
2026-04-16 14:01:34 -0700

    LPD-79040 SF Test

--- modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/shipment/test/CommerceShipmentLocalServiceTest.java
+++ modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/shipment/test/CommerceShipmentLocalServiceTest.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -13,13 +13,14 @@ import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
 import com.liferay.commerce.model.CommerceAddress;
 import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.commerce.model.CommerceShipment;
-import com.liferay.commerce.model.CommerceShippingFixedOption;
 import com.liferay.commerce.model.CommerceShippingMethod;
 import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.test.util.CPTestUtil;
 import com.liferay.commerce.service.CommerceOrderLocalService;
 import com.liferay.commerce.service.CommerceShipmentLocalService;
 import com.liferay.commerce.shipment.test.util.CommerceShipmentTestUtil;
+import com.liferay.commerce.shipping.engine.fixed.model.CommerceShippingFixedOption;
 import com.liferay.commerce.test.util.CommerceInventoryTestUtil;
 import com.liferay.commerce.test.util.CommerceTestUtil;
 import com.liferay.petra.string.StringPool;
@@ -27,6 +28,7 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
@@ -35,10 +37,11 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
 import java.math.BigDecimal;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -57,7 +60,7 @@ public class CommerceShipmentLocalServiceTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new AggregateTestRule(
 			new LiferayIntegrationTestRule(),
-			PermissionCheckerMethodTestRule.getPermissionCheckerMethodTestRule());
+			PermissionCheckerMethodTestRule.INSTANCE);
 
 	@Before
 	public void setUp() throws Exception {
@@ -68,27 +71,47 @@ public class CommerceShipmentLocalServiceTest {
 			_group.getCompanyId());
 
 		_commerceChannel = CommerceTestUtil.addCommerceChannel(
-			_user.getUserId(), _group.getGroupId(), _commerceCurrency.getCode());
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		for (CommerceOrder commerceOrder : _commerceOrders) {
-			CommerceTestUtil.deleteCommerceOrder(
-				commerceOrder.getCommerceOrderId());
-		}
-
-		CommerceTestUtil.deleteCommerceChannel(_commerceChannel);
-
-		CommerceCurrencyTestUtil.deleteCommerceCurrency(_commerceCurrency);
-
-		UserTestUtil.deleteUser(_user);
-
-		GroupTestUtil.deleteGroup(_group);
+			_group.getGroupId(), _commerceCurrency.getCode());
 	}
 
 	@Test
-	public void testShipmentsAreNotDuplicatedForEachOrderItem() throws Exception {
+	public void testGetCommerceShipments() throws Exception {
+		CommerceOrder commerceOrder = _addCommerceOrderWithShipment();
+
+		List<CommerceShipment> commerceShipments =
+			_commerceShipmentLocalService.getCommerceShipments(
+				commerceOrder.getCommerceOrderId(), QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS);
+
+		Assert.assertEquals(
+			commerceShipments.toString(), 1, commerceShipments.size());
+	}
+
+	@Test
+	public void testGetCommerceShipmentsCount() throws Exception {
+		CommerceOrder commerceOrder = _addCommerceOrderWithShipment();
+
+		int count = _commerceShipmentLocalService.getCommerceShipmentsCount(
+			commerceOrder.getCommerceOrderId());
+
+		Assert.assertEquals(1, count);
+	}
+
+	@Test
+	public void testGetCommerceShipmentStatusesByCommerceOrderId()
+		throws Exception {
+
+		CommerceOrder commerceOrder = _addCommerceOrderWithShipment();
+
+		int[] statuses =
+			_commerceShipmentLocalService.
+				getCommerceShipmentStatusesByCommerceOrderId(
+					commerceOrder.getCommerceOrderId());
+
+		Assert.assertEquals(Arrays.toString(statuses), 1, statuses.length);
+	}
+
+	private CommerceOrder _addCommerceOrderWithShipment() throws Exception {
 		CommerceOrder commerceOrder = CommerceTestUtil.addB2CCommerceOrder(
 			_user.getUserId(), _commerceChannel.getGroupId(),
 			_commerceCurrency.getCommerceCurrencyId());
@@ -96,13 +119,17 @@ public class CommerceShipmentLocalServiceTest {
 		CommerceInventoryWarehouse commerceInventoryWarehouse =
 			CommerceInventoryTestUtil.addCommerceInventoryWarehouse();
 
+		_commerceInventoryWarehouses.add(commerceInventoryWarehouse);
+
 		CommerceTestUtil.addWarehouseCommerceChannelRel(
 			commerceInventoryWarehouse.getCommerceInventoryWarehouseId(),
 			_commerceChannel.getCommerceChannelId());
 
 		for (int i = 0; i < 7; i++) {
-			CPInstance cpInstance = CommerceTestUtil.createCPInstance(
-				_user.getUserId(), _group.getGroupId());
+			CPInstance cpInstance = CPTestUtil.addCPInstanceWithRandomSku(
+				_group.getGroupId());
+
+			_cpInstances.add(cpInstance);
 
 			CommerceTestUtil.addCommerceOrderItem(
 				commerceOrder.getCommerceOrderId(),
@@ -154,7 +181,8 @@ public class CommerceShipmentLocalServiceTest {
 		commerceOrder.setShippingOptionName(
 			commerceShippingFixedOption.getNameCurrentValue());
 
-		_commerceOrderLocalService.updateCommerceOrder(commerceOrder);
+		commerceOrder = _commerceOrderLocalService.updateCommerceOrder(
+			commerceOrder);
 
 		_commerceOrders.add(commerceOrder);
 
@@ -163,32 +191,40 @@ public class CommerceShipmentLocalServiceTest {
 				commerceOrder.getGroupId(), commerceOrder.getCommerceOrderId(),
 				commerceInventoryWarehouse.getCommerceInventoryWarehouseId());
 
-		List<CommerceShipment> commerceShipments =
-			_commerceShipmentLocalService.getCommerceShipments(
-				commerceOrder.getCommerceOrderId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS);
+		_commerceShipments.add(commerceShipment);
 
-		Assert.assertEquals(1, commerceShipments.size());
-		Assert.assertEquals(
-			commerceShipment.getCommerceShipmentId(),
-			commerceShipments.get(0).getCommerceShipmentId());
-
-		int count = _commerceShipmentLocalService.getCommerceShipmentsCount(
-			commerceOrder.getCommerceOrderId());
-
-		Assert.assertEquals(1, count);
+		return commerceOrder;
 	}
 
+	@DeleteAfterTestRun
 	private CommerceChannel _commerceChannel;
+
+	@DeleteAfterTestRun
 	private CommerceCurrency _commerceCurrency;
-	private List<CommerceOrder> _commerceOrders = new ArrayList<>();
-	private Group _group;
-	private User _user;
+
+	@DeleteAfterTestRun
+	private List<CommerceInventoryWarehouse> _commerceInventoryWarehouses =
+		new ArrayList<>();
 
 	@Inject
 	private CommerceOrderLocalService _commerceOrderLocalService;
 
+	@DeleteAfterTestRun
+	private List<CommerceOrder> _commerceOrders = new ArrayList<>();
+
 	@Inject
 	private CommerceShipmentLocalService _commerceShipmentLocalService;
+
+	@DeleteAfterTestRun
+	private List<CommerceShipment> _commerceShipments = new ArrayList<>();
+
+	@DeleteAfterTestRun
+	private List<CPInstance> _cpInstances = new ArrayList<>();
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@DeleteAfterTestRun
+	private User _user;
 
 }
